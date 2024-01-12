4efee5db90dca2685b7095248f08e7c150baa4e3
Javier Pedroza <jpedroza@spreedly.com>
2024-01-12 08:16:44 -0500

    update migration

--- db/migrate/20240111014258_change_closing_date_to_date_in_transactions.rb
+++ db/migrate/20240111014258_change_closing_date_to_date_in_transactions.rb
@@ -1,5 +1,14 @@
 class ChangeClosingDateToDateInTransactions < ActiveRecord::Migration[6.1]
-  def change
-    change_column :transactions, :closing_date, :date
+  def up
+    # Add a default value for existing NULL values, if necessary
+    change_column_default :transactions, :closing_date, -> { 'CURRENT_DATE' }
+
+    # Change the data type of the column using the USING clause
+    change_column :transactions, :closing_date, 'date USING CAST(closing_date AS date)'
+  end
+
+  def down
+    # Change the data type back to its original type if you need to rollback the migration
+    change_column :transactions, :closing_date, :original_data_type
   end
 end
